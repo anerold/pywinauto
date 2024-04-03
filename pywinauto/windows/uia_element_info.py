ed34024c78f7d03a2ef83be9668f15d24739c56a
nerolda <adam.nerold@stringdata.cz>
2024-04-03 15:37:53 +0200

    licence

--- pywinauto/windows/uia_element_info.py
+++ pywinauto/windows/uia_element_info.py
@@ -332,7 +332,22 @@ class UIAElementInfo(ElementInfo):
         
     @property
     def item_type(self):
-        """StringData ANE 3.4.2024: return ItemType of the element"""
+        """return ItemType of the element
+        
+        Copyright 2024 StringData s.r.o.
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+        """
         try:
             val = self._element.CurrentItemType
             return text_type('') if val is None else val
