4105700699362d00f50fdd9a0a80ebd7e19e8a15
nerolda <adam.nerold@stringdata.cz>
2024-04-04 10:17:11 +0200

    added 'Selected' property

--- pywinauto/windows/uia_defines.py
+++ pywinauto/windows/uia_defines.py
@@ -81,8 +81,23 @@ class IUIA(object):
             self.known_control_type_ids[type_id] = ctrl_type
 
     def build_condition(self, process=None, class_name=None, name=None, control_type=None,
-                        content_only=None):
-        """Build UIA filtering conditions"""
+                        content_only=None, selected=None):
+        """Build UIA filtering conditions
+        
+        Copyright 2024 StringData s.r.o.
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+        """
         conditions = []
         if process:
             conditions.append(self.iuia.CreatePropertyCondition(self.UIA_dll.UIA_ProcessIdPropertyId, process))
@@ -96,6 +111,10 @@ class IUIA(object):
             elif not isinstance(control_type, int):
                 raise TypeError('control_type must be string or integer')
             conditions.append(self.iuia.CreatePropertyCondition(self.UIA_dll.UIA_ControlTypePropertyId, control_type))
+
+        if selected is not None:
+            # StringData customization
+            conditions.append(self.iuia.CreatePropertyCondition(self.UIA_dll.UIA_SelectionItemIsSelectedPropertyId, selected))
 
         if name:
             # TODO: CreatePropertyConditionEx with PropertyConditionFlags_IgnoreCase
--- pywinauto/windows/uia_element_info.py
+++ pywinauto/windows/uia_element_info.py
@@ -354,6 +354,32 @@ class UIAElementInfo(ElementInfo):
         except COMError:
             # probably element already doesn't exist
             return text_type('')
+        
+    @property
+    def selected(self):
+        """return Selected of the element
+        
+        Copyright 2024 StringData s.r.o.
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+        """
+        #https://learn.microsoft.com/en-us/windows/win32/winauto/uiauto-control-pattern-propids
+        try:
+            val = self._element.GetCurrentPropertyValue(30079)
+            return val
+        except COMError:
+            # probably element already doesn't exist
+            return None
 
     @property
     def legacy_action(self):
